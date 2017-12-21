032274f6c02d976bd4eeb9f235dc2fef3d2d6ebe
luciano <luciano.zapata314@gmail.com>
2017-12-21 11:28:29 -0300

    Arreglado el trigger imputar_pago2.sql  para que compare la fecha del movimiento y no la actual

--- Arabidopsis/db/trigger imputar_pago2.sql
+++ Arabidopsis/db/trigger imputar_pago2.sql
@@ -9,7 +9,7 @@ CREATE   TRIGGER  IMPUTAR_PAGOS2
 
    IF(NEW.numero_cuota = 1) THEN
 		SET @FECHA_SALIDA:=(SELECT M.fecha from movimientos M inner join pagos_realizados P on P.movimiento_id=M.id where P.id=NEW.pago_realizado_id);
-		SET @FECHA:= (SELECT fecha_vencimiento from tarjeta_de_creditos where id=NEW.tarjeta_id and (date_format(`fecha_vencimiento`, '%Y-%m') = date_format(curdate(), '%Y-%m')) and date_format(`fecha_vencimiento`, '%d') >= date_format(curdate(), '%d'));
+		SET @FECHA:= (SELECT fecha_vencimiento from tarjeta_de_creditos where id=NEW.tarjeta_id and (date_format(`fecha_vencimiento`, '%Y-%m') = date_format(@FECHA_SALIDA, '%Y-%m')) and date_format(`fecha_vencimiento`, '%d') >= date_format(@FECHA_SALIDA, '%d'));
 		
 		IF(@FECHA IS NOT NULL) THEN
 			SET NEW.fecha_imputacion = @FECHA;
